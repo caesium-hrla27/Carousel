c854cd87484307d41d30a14faf41fdd9cc7b54ce
Jennifer Haren <jennifer.haren110@gmail.com>
2019-02-13 11:03:43 -0800

    Move styled-components to separate file
    Created a separated file, styles.js for styled-components.  Exported all css styles and imported into parent React component file.

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -2,104 +2,10 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Shoe from './Shoe.jsx';
 import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyles = createGlobalStyle`
-  body {
-    @import url('https://fonts.googleapis.com/css?family=Heebo');
-    font-family: 'Heebo', sans-serif;
-  }
-`;
+import { GlobalStyles, Wrapper, Ul, Div, ButtonLeft, ButtonRight, Bar, DivBar } from './style.js';
 
-const Wrapper = styled.div`
-  width: 100%;
-  overflow: hidden;
-`;
 
-const Ul = styled.ul`
-  list-style-type: none;
-  display: flex;
-  margin: 0 0 0 0;
-  padding: 0px 40px;
-  transition: ${(props) => props.sliding ? 'none' : 'transform 350ms ease-in-out 0s'};
-  transform: ${(props) => {
-    if (!props.sliding) {
-      return 'translateX(calc(-30% - 20px))';
-    }
-    if (props.direction === 'prev') {
-      return 'translateX(calc(2 * -30% - 20px))';
-    }
-    return 'translateX(0%)';
-  }}
-`;
-
-const Div = styled.div`
-  width: 100%;
-  padding: 0px 25px;
-`;
-
-const ButtonLeft = styled.button`
-  background-color: rgba(232, 232, 232, 0.3);
-  transition: background-color 0.5s ease-in-out;
-  border: black;
-  color: rgb(109, 109, 109);
-  padding: 15px 17px 10px 17px;
-  border-radius: 50%;
-  position: absolute;
-  left: 35px;
-  top: 330px;
-
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.9);
-  } 
-
-  -moz-transform: scaleX(-1);
-    -o-transform: scaleX(-1);
-    -webkit-transform: scaleX(-1);
-    transform: scaleX(-1);
-
-`;
-
-const ButtonRight = styled.button`
-  background-color: rgba(232, 232, 232, 0.3);
-  transition: background-color 0.5s ease-in-out;
-  border: black;
-  color: rgb(109, 109, 109);
-  padding: 15px 17px 10px 17px;
-  border-radius: 50%;
-  position: absolute;
-  right: 35px;
-  top: 330px;
-
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.9);
-  } 
-`;
-
-const DivBar = styled.div`
-  position: relative;
-  width: 95%;
-  margin: 40px 30px 40px 30px;  
-  border-bottom: 2px solid rgba(109, 109, 109, 0.25);
-  z-index: 1;
-  border-radius: 2px;
-`;
-
-const Bar = styled.div`
-  width: 28%;
-  border-bottom: 2px solid black;
-  z-index: 2;
-  position: absolute;
-  transition: ${(props) => props.clickPosition > 0 ? 'left 0.5s' : 'none'};
-
-  left: ${(props) => {
-    console.log('this is the rightclick', props.clickPosition);
-    return props.clickPosition === 1 ? '111px' : props.clickPosition === 2 ? '222px' : props.clickPosition === 3 ? '333px' : 
-      props.clickPosition === 4 ? '444px' : props.clickPosition === 5 ? '555px' : props.clickPosition === 6 ? '666px' : 
-        props.clickPosition === 7 ? '777px' : props.clickPosition === 8 ? '890px' : props.clickPosition === 9 ? '1000px' : '0px';
-  }
-}}
-`;
 
 class Carousel extends Component {
   constructor(props) {
--- dummyData.js
+++ dummyData.js
@@ -1,76 +1,329 @@
-const shoes = [
-  {
-    name: 'Nike Zoom KD11 BHM',
-    price: 150,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe1.jpeg',
-    category: 'men_athletic'
-  }, 
-  {
-    name: 'Nike Air Versitile III',
-    price: 66.97,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe2.jpeg',
-    category: 'men_athletic'
-  },   	  
-  {
-    name: 'Nike Air Precision II FlyEase',
-    price: 70,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe3.jpeg',
-    category: 'men_athletic'
-  },  	  
-  {
-    name: 'Nike KD Trey 5 VI',
-    price: 90,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe4.jpeg',
-    category: 'men_athletic'
-  },  
-  {
-    name: 'LeBron Witness III',
-    price: 90,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe5.jpeg',
-    category: 'men_athletic'
-  },       
-  {
-    name: 'Nike Air Max Infuriate 2 Mid',
-    price: 85,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe6.jpeg',
-    category: 'men_athletic'
-  },   	    	  
-  {
-    name: 'Nike Air Precision II FlyEase',
-    price: 70,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe7.jpeg',
-    category: 'men_athletic'
-  },   	    	  
-  {
-    name: 'Nike Air Precision II FlyEase',
-    price: 70,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe8.jpeg',
-    category: 'men_athletic'
-  },   	  
-  {
-    name: 'Nike Fly.By Low II',
-    price: 65,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe9.jpeg',
-    category: 'men_athletic'
-  },   	  
-  {
-    name: 'Nike Zoom KD11 BHM',
-    price: 150,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe10.jpeg',
-    category: 'men_athletic'
-  },   	    	  
-  { 
-    name: 'Air Jordan 1 Mid SE',
-    price: 120,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe11.jpeg',
-    category: 'men_athletic'
-  },   	    	  
-  {
-    name: 'Kyrie 5',
-    price: 130,
-    shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe12.jpeg',
-    category: 'men_athletic'
-  }   	  
-];
+// const shoes = [
+// 1  {
+//     name: 'Nike Zoom KD11 BHM',
+//     price: 150,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe1.jpeg',
+//     category: 'men_athletic'
+//   }, 
+//  2 {
+//     name: 'Nike Air Versitile III',
+//     price: 66.97,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe2.jpeg',
+//     category: 'men_athletic'
+//   },   	  
+//  3 {
+//     name: 'Nike Air Precision II FlyEase',
+//     price: 70,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe3.jpeg',
+//     category: 'men_athletic'
+//   },  	  
+//  4 {
+//     name: 'Nike KD Trey 5 VI',
+//     price: 90,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe4.jpeg',
+//     category: 'men_athletic'
+//   },  
+//  5 {
+//     name: 'LeBron Witness III',
+//     price: 90,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe5.jpeg',
+//     category: 'men_athletic'
+//   },       
+//  6 {
+//     name: 'Nike Air Max Infuriate 2 Mid',
+//     price: 85,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe6.jpeg',
+//     category: 'men_athletic'
+//   },   	    	  
+//  7 {
+//     name: 'Nike Air Precision II FlyEase',
+//     price: 70,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe7.jpeg',
+//     category: 'men_athletic'
+//   },   	    	  
+//  8 {
+//     name: 'Air Jordan Future',
+//     price: 104.97 - on sale from 150,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-AIR-JORDAN-FUTURE.jpeg',
+//     category: 'men_athletic'
+//   },   	  
+//  9 {
+//     name: 'Jordan Melo 1.5 SE',
+//     price: 97.97 - on sale from 140,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-JORDAN-MELO.jpeg',
+//     category: 'men_athletic'
+//   },   	  
+//  10 {
+//     name: 'Nike Zoom KD11 BHM',
+//     price: 150,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe10.jpeg',
+//     category: 'men_athletic'
+//   },   	    	  
+//  11 { 
+//     name: 'Air Jordan 1 Mid SE',
+//     price: 120,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe11.jpeg',
+//     category: 'men_athletic'
+//   },   	    	  
+//  12 {
+//     name: 'Kyrie 5',
+//     price: 130,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/shoe12.jpeg',
+//     category: 'men_athletic'
+//   },         
+//  13 {
+//     name: 'Air Jordan XXXIII',
+//     price: 175,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-AIR-JORDAN-XXXIII.jpeg',
+//     category: 'men_athletic'
+//   },   
+//  14 {
+//     name: 'Kobe AD',
+//     price: 140,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-KOBE-AD.jpeg',
+//     category: 'men_athletic'
+//   },       
+// 15 {
+//     name: 'LeBron 16',
+//     price: 185,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-LEBRON-16.jpeg',
+//     category: 'men_athletic'
+//   }, 
 
-module.exports.shoes = shoes;
+// 16 {
+//     name: 'Kyrie Low',
+//     price: 98.97, - on sale from 110
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-KYRIE-LOW.jpeg',
+//     category: 'men_athletic'
+//   },      
+
+// 17 {
+//     name: 'Nike Air Force Max',
+//     price: 160,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-AIR-FORCE.jpeg',
+//     category: 'men_athletic'
+//   },      
+
+// 18 {
+//     name: 'PG 2.5 Pendleton',
+//     price: 120,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-PENDLETON.jpeg',
+//     category: 'men_athletic'
+//   },      
+
+// 19 {
+//     name: 'Nike Mamba Rage',
+//     price: 100,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-MAMBA.jpeg',
+//     category: 'men_athletic'
+//   },      
+
+// 20 {
+//     name: 'Kyrie 4 (Team)',
+//     price: 95.97 - on sale from 120,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-Kyrie-4.jpeg',
+//     category: 'men_athletic'
+//   },     
+
+// 21 {
+//     name: 'PG 3 BHM',
+//     price: 110,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-PG-3.jpeg',
+//     category: 'men_athletic'
+//   },
+
+// 21 {
+//     name: 'Kyrie 5 CNY',
+//     price: 130,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-KYRIE-5-CNY.jpeg',
+//     category: 'men_athletic'
+//   },  
+
+// 21 {
+//     name: 'Jordan "Why Not?" Zer0.2',
+//     price: 125,
+//     shoeUrl: 'https://images.nike.com/is/image/DotCom/AV2296_001?$NIKE_PWP_GRAY$&wid=420&hei=420',
+//     category: 'men_athletic'
+//   },      
+
+// 22 {
+//     name: 'Jordan Max Aura',
+//     price: 120,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-JORDAN-MAX-AURA.jpeg',
+//     category: 'men_athletic'
+//   },
+
+// 23 {
+//     name: 'Jordan "Why Not?" ZER0.1',
+//     price: 94.97 - ON SALE FROM 125,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-ZERO-1.jpeg',
+//     category: 'men_athletic'
+//   },      
+
+// 24 {
+//     name: 'Jordan "Why Not?" ZER0.1 Low',
+//     price: 79.97 - ON SALE FROM 115,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-WHY-ZERO-LOW.jpeg',
+//     category: 'men_athletic'
+//   },      
+
+// 25 {
+//     name: 'Jordan "Why Not?" ZER0.1 CHAOS',
+//     price: 92.97 - ON SALE FROM 125,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/M-A-JORDAN-CHAOS.jpeg',
+//     category: 'men_athletic'
+//   },
+
+// // WOMEN'S SHOE
+
+// 51 {
+//     name: 'Nike Outburst',
+//     price: 90,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-OUTBURST.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 52 {
+//     name: 'Nike Internationalist',
+//     price: 90,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-INTERNATIONALIST.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 53 {
+//     name: 'Nike Classic Cortez',
+//     price: 70,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-CLASSIC-CORTEZ.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 54 {
+//     name: 'Nike Blazer Low Lux Premium',
+//     price: 85,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-BLAZER-PREMIUM.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 55 {
+//     name: 'Nike Classic Cortez Premium',
+//     price: 90,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-CORTEZ-PREMIUM.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 56 {
+//     name: 'Nike Outburst Premium',
+//     price: 100,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-OUTBURST-PREMIUM.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 57 {
+//     name: 'Nike Air Max 1',
+//     price: 110,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-AIR-MAX-1.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 58 {
+//     name: 'Nike EXP-X14-Premium',
+//     price: 120,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-EXP-PREMIUM.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 59 {
+//     name: 'Nike Air Max Sequent 4',
+//     price: 100,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-SEQUENT-4.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 60 {
+//     name: 'Nike Tanjun',
+//     price: 65,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-TANJUN.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 61 {
+//     name: 'Nike Air Huarache City Move',
+//     price: 90,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-HUARACHE-CITY-MOVE.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 62 {
+//     name: 'Nike Blazer Mid Vintage',
+//     price: 85,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-MID-VINTAGE.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 63 {
+//     name: 'Nike Blazer Mid Premium',
+//     price: 89.97 - on sale from 100,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-MID-PREMIUM.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 64 {
+//     name: 'Nike Blazer Mid',
+//     price: 100,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-BLAZER-MID.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 65 {
+//     name: 'Nike SB Bruin High',
+//     price: 85,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-SB-BRUIN-HIGH.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 66 {
+//     name: 'Nike Air Force 1 High Utility',
+//     price: 110,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-AIR-FORCE-1-HIGH-UTILITY.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 67 {
+//     name: 'Nike Blazer Low Suede',
+//     price: 66.97 - on sale from 75,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-BLAZER-LOW-SUEDE.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 68 {
+//     name: 'Nike Vandal High LX',
+//     price: 110,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-VANDAL-HIGH-LX.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 69 {
+//     name: 'Nike Air Force 1 Sage Low',
+//     price: 100,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-AIR-FORCE-SAGE-LOW.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 70 {
+//     name: 'Nike Air Max 90',
+//     price: 100,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-AIR-MAX-90.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+// 71 {
+//     name: 'Nike Air Force 1 Jester High XX',
+//     price: 130,
+//     shoeUrl: 'https://s3.us-east-2.amazonaws.com/carousel-fec/W-L-AIR-FORCE-1-JESTER-HIGH.jpeg',
+//     category: 'women_lifestyle'
+//   },
+
+
+// ];
+
+// module.exports.shoes = shoes;
