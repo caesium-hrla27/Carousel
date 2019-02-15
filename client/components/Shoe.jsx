256fa65e4af1bc6ef290d22e88ab9992e836e208
Jennifer Haren <jennifer.haren110@gmail.com>
2019-02-15 11:16:46 -0800

    Minor refactoring

--- client/components/Shoe.jsx
+++ client/components/Shoe.jsx
@@ -10,23 +10,30 @@ const Shoe = (props) => {
   let randomColor = props.color();
 
   return (
+
     <Li order={props.order} onMouseEnter={() => props.mouseEnter(props.reviewsNum)} onMouseLeave={() => props.mouseLeave()}>
+      
       <img src={props.shoeUrl} /><br />
+      
       <Text>
+
         <ShoeDiv>
           {props.reviewsNum && props.isHovering ? <Reviews reviewsNum={props.reviewsNum} reviewsAvg={props.reviewsAvg} /> : 
             <Color>{randomColor} {randomColor === 1 ? 'Color' : 'Colors'}<br /></Color>} 
           {props.name}<br />
         </ShoeDiv>
         {props.category === 'men_athletic' ? 'Basketball Shoe' : props.category}<br />
+        
         <Prices>
           <OrigPrice origPrice={props.price} salePrice={props.salePrice}>
             {`$${props.price}`}
           </OrigPrice>
           {props.salePrice ? `$${props.salePrice}` : null}<br />
         </Prices>
+
       </Text>
     </Li>
+
   );
 };
 
