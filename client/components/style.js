24ed0cdd4a46fe54aaf368a24a6b925b9100d0a5
Jennifer Haren <jennifer.haren110@gmail.com>
2019-02-15 11:07:53 -0800

    Move styled-components to styles.js file

--- client/components/style.js
+++ client/components/style.js
@@ -125,3 +125,44 @@ export const ShoeDiv = styled.div`
   margin-bottom: 
 `;
 
+// REVIEWS STYLES
+
+export const StylesDiv = styled.div`
+  position: relative;
+  top: 0;
+  left: 0;
+  font-size: 13px;
+  border-bottom: 1px solid rgba(109, 109, 109, 0.5);
+  padding: 5px 0px;
+  margin: 5px 0px 10px; 
+`;
+
+export const StarRatingsTop = styled.div`
+  display: inline-block;
+  position: relative;
+
+  &:before {
+    content: '★ ★ ★ ★ ★';
+    color: rgba(109, 109, 109, 0.25);
+  }
+`;
+
+
+export const StarRatingsBottom = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  white-space: nowrap;
+  overflow: hidden;
+  width: ${(props) => {
+    let starsNum = (props.reviewsAvg / 5) * 100;
+    return `${starsNum}%`;   
+  }}
+
+  &:before {
+    content: '★ ★ ★ ★ ★';
+    color: black; 
+  }
+ `;
+
+
