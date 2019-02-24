89fd8f5f021c55af32c840c715440fd600c37cde
Jennifer Haren <jennifer.haren110@gmail.com>
2019-02-24 14:49:54 -0800

    Refactor for CSS modules
    Needed to delete references to styled-components so it will not load and interfere with other team member's instance.

--- client/components/Indicator.jsx
+++ client/components/Indicator.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import IndicatorStyles from '../styles/IndicatorStyles.css';
 
--- client/components/Prices.jsx
+++ client/components/Prices.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import PricesStyles from '../styles/PricesStyles.css';
 
